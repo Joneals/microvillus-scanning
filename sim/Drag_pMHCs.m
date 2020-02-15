743c1519cf0ebe3eede5faa8376768d70054503d
Jacob Reynolds <joreynolds97@gmail.com>
2020-02-14 20:24:51 -0500

    Made drag forces accurate/equal to existing bond forces

--- sim/Drag_pMHCs.m
+++ sim/Drag_pMHCs.m
@@ -1,5 +1,5 @@
-k_spring = 0.1;
-u_drag = 2;
+k_spring = k_bond; % [pN/nm]
+u_drag = 2; % [pN/(um/min)]
 
 for iBond = 1:size(CC_SlipBond,1)
     E1_Velocity = zeros(length(CC_E1),2);
@@ -7,9 +7,10 @@ for iBond = 1:size(CC_SlipBond,1)
         continue;
     end
     
-    vBond = CC_SlipBond(iBond,3:4) - CC_SlipBond(iBond,1:2); % Vector from pMHC -> TCR
+    vBond = [CC_SlipBond(iBond,3:4) - CC_SlipBond(iBond,1:2) Height]; % Vector from pMHC -> TCR, 3d, includes height (nm)
     pMHC = CC_SlipBond(iBond,5); % pMHC number
-    vForceBond = k_spring .* vBond; % Force vector on pMHC - Need to be squared instead of linear?
+%              Spring const   delta from natural length    Only xy comp
+    vForceBond = k_spring .* ( norm(vBond) - z_bond ) .* ( norm(vBond(1:2))./norm(vBond) ) .* ( vBond(1:2)./norm(vBond(1:2)) ); % Force vector (pN)
     E1_Velocity(pMHC,1:2) = vForceBond ./ u_drag; % Assume no acceleration
 
     CC_E1(pMHC,1:2) = CC_E1(pMHC,1:2) + E1_Velocity(pMHC,1:2) .* CF .* dt; % Update position based on velocity
@@ -22,9 +23,9 @@ for iBond = 1:size(CC_CatchBond,1)
         continue;
     end
     
-    vBond = CC_CatchBond(iBond,3:4) - CC_CatchBond(iBond,1:2); % Vector from pMHC -> TCR
+    vBond = [ CC_CatchBond(iBond,3:4) - CC_CatchBond(iBond,1:2) Height ]; % Vector from pMHC -> TCR
     pMHC = CC_CatchBond(iBond,5); % pMHC number
-    vForceBond = k_spring .* vBond; % Force vector on pMHC - Need to be squared instead of linear?
+    vForceBond = k_spring .* ( norm(vBond) - z_bond ) .* ( norm(vBond(1:2))./norm(vBond) ) .* ( vBond(1:2)./norm(vBond(1:2)) ); % Force vector
     Ag_Velocity(pMHC,1:2) = vForceBond ./ u_drag; % Assume no acceleration
 
     CC_Ag(pMHC,1:2) = CC_Ag(pMHC,1:2) + Ag_Velocity(pMHC,1:2) .* CF .* dt; % Update position based on velocity
